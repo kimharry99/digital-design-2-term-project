9d23a11ae78bcc54b2d49434b96a39abeaa28e21
kimharry99 <kimharry99@korea.ac.kr>
2019-05-04 16:20:27 +0900

    delete unused variables

--- main.v
+++ main.v
@@ -1,38 +1,38 @@
 module finalTermProject(
 	input [4:0] SW,
 	input CLOCK_50,
-	output reg [0:6] HEX3,HEX1,HEX0,HEX4,HEX7
+	output reg [0:6] HEX3,HEX1,HEX0
 	);
 	
 	wire in_pulse, pulse;
 	wire input_x,input_y,in_A,in_B,in_C;
 	wire newClk_24;
 	wire A,B,C;
-	wire [0:6] h0,h1,h2,h3,h4,h5,h6,h7;
+	wire [0:6] h0,h1,h3;
 	reg state_A, state_B, state_C;
 	
+	input_pulse(SW[0],SW[1],SW[2],SW[3],in_pulse);
+	
+	//correct hardware error
 	d_flip_flop ( in_pulse ,newClk_24 ,1'b0 , pulse );
 	
-	input_pulse(SW[0],SW[1],SW[2],SW[3],in_pulse);
 	input_encoder(SW[0],SW[1],SW[2],SW[3],input_x,input_y);
 	clock_24(CLOCK_50,newClk_24);
 	
 	conventionalLogic(A,B,C,input_x,input_y, in_A,in_B,in_C);
 	dffs(in_A,in_B,in_C,pulse,SW[4],A,B,C);
 	
-	print(A,B,C,pulse,h0,h1,h3,h4,h7);
+	print(A,B,C,h0,h1,h3);
 	
 	always@(*)
 	begin
 		HEX0<=h0;
 		HEX1<=h1;
 		HEX3<=h3;
-		HEX4<=h4;
-		HEX7<=h7;
 	end
 endmodule
 
-//design the module that output  1 when 0000>0001
+/* design the module that output  1 when 0000>0001 */
 module input_pulse(
 input SW0,SW1,SW2,SW3,
 output pulse
@@ -41,6 +41,7 @@ assign pulse=SW0|SW1|SW2|SW3;
 
 endmodule
 
+/* change input data to 2bit */
 module input_encoder(
 input SW0,SW1,SW2,SW3,
 output encodedOutput_1,encodedOutput_0
@@ -58,6 +59,7 @@ assign b = A & x & y | ~A & ~B & ~x & y | ~A & ~B & C & ~x | B & ~C & ~x & ~y | 
 assign c = C & y | A & C | ~A & ~C & ~y | ~C & ~x & ~y | B & x & ~y;
 endmodule
 
+
 module d_flip_flop ( input din ,input clk ,input reset ,output reg dout );
 	initial
 	begin
@@ -72,6 +74,7 @@ module d_flip_flop ( input din ,input clk ,input reset ,output reg dout );
 	end
 endmodule 
 
+/* slow clock */
 module clock_24(input clk, output newclk);
 	reg [24:0]cnt;
 	
@@ -82,6 +85,7 @@ module clock_24(input clk, output newclk);
 	assign newclk = cnt[20];
 endmodule
 
+/* thee dff in one module */
 module dffs(
 input dinA, dinB, dinC, clk, reset,
 output doutA, doutB, doutC
@@ -92,24 +96,18 @@ d_flip_flop (dinA, clk, reset, doutA);
 d_flip_flop (dinB, clk, reset, doutB);
 d_flip_flop (dinC, clk, reset, doutC);
 
-
 endmodule
 
+/* print data to hex */
 module print(
-	input A,B,C,pulse,
-	output reg [0:6] h0,h1,h3,h4,h7
+	input A,B,C,
+	output reg [0:6] h0,h1,h3
 );
 	parameter Seg9 = 7'b000_1100; parameter Seg8 = 7'b000_0000; parameter Seg7 = 7'b000_1111; 
 	parameter Seg6 = 7'b010_0000; parameter Seg5 = 7'b010_0100;	parameter Seg4 = 7'b100_1100; 
 	parameter Seg3 = 7'b000_0110; parameter Seg2 = 7'b001_0010; parameter Seg1 = 7'b100_1111; 
 	parameter Seg0 = 7'b000_0001; parameter SegX = 7'b111_1111;
-	always@(*)
-	begin
-		case(pulse)
-			1:h4 = Seg1;
-			0:h4 = Seg0;
-		endcase
-	end
+	
 	always@(*)
 	begin
 		case({ A,B,C })
@@ -138,19 +136,6 @@ module print(
 			1: h3 = Seg0;
 			0: h3 = Seg0;
 			default: h3 = SegX;
-		endcase
-		case({ A,B,C })
-			9: h7 = Seg9;
-			8:h7 = Seg8;
-			7:h7 = Seg7;
-			6:h7 = Seg6;
-			5:h7 = Seg5;
-			4:h7 = Seg4;
-			3:h7 = Seg3;
-			2:h7 = Seg2;
-			1:h7 = Seg1;
-			0:h7 = Seg0;
-			default: h7 = SegX;
 		endcase
 	end
 endmodule
